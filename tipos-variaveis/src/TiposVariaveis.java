731c1e77191d3c476e9e517f8b6f5e6e64c211dc
Joao Neto <joao-antoniopn@live.com>
2023-02-28 21:24:37 -0300

    Casting

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,12 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        byte idade = 123;
-        short ano = 2021;
-        int cep = 21070333;
-        long cpf = 98765432109L;
-        float pi = 3.14F;
-        double salarioMinimo = 2.500;
 
-        System.out.println("Inicializado o projeto.");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        // casting
+        short numeroCurto2 = (short) numeroNormal;
+
     }
 }
