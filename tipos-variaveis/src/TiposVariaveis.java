c69bd4cb8654413468ca1958636d07e2c2c50edf
Joao Neto <joao-antoniopn@live.com>
2023-02-28 21:22:01 -0300

    tipos de dados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salarioMinimo = 2.500;
+
         System.out.println("Inicializado o projeto.");
     }
 }
